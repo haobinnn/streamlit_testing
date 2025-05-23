a82def5e3620d6ec33e076ff98cd00880f5d72ba
Jan Earl F. Rodriguez <janearlrodriguez1209@gmail.com>
2025-05-24 00:14:23 +0800

    Add files via upload

---
+++ streamlit_test_texting.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Hello Streamlit!")
+st.write("This is a test of my Streamlit setup for Build with AI 2025.")
+
+name = st.text_input("What's your name?")
+if name:
+    st.write(f"Hello, {name}!")
+
+pangit = st.text_input("Pangit ka sabihin mo bakit")
+if pangit:
+    st.write("omsim shongit ka tlga")
+
+st.title("Okay ito serious mode, HAPPY BIRTHDAY MISHA")
+st.write("Testing ko ito kasi pupunta ako ng Build AI bukas.")
+
+name = st.text_input("What's your name?")
+if name:
+    st.write(f"Hello, {name}! Alam mo bang birthday ni Misha?")
+else name == Misha || MISHA || misha:
+    st.write(f"HAPPY BIRTHDAY MISHA!!")
+
+pangit = st.text_input("Pangit ka sabihin mo bakit")
+if pangit:
+    st.write("omsim shongit ka tlga kasi {pangit}")
